2c67d1f9c02de2fc5ec2d6fb3c93fe964dd67663
HenriquePedro2905 <136708570+HenriquePedro2905@users.noreply.github.com>
2024-07-18 17:48:17 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 let id;
+const taskDiv = document.getElementById('taskDiv');
 
 document.addEventListener('DOMContentLoaded', () => {
     const formNewTask = document.getElementById('formNewTask');
@@ -8,14 +9,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const name = document.getElementById('name').value;
         const description = document.getElementById('description').value;
-        const dateConlusion = document.getElementById('dateConlusion').value;
+        const dateInput = document.getElementById('dateConlusion').value;
         const priority = document.getElementById('priority').value;
         const status = false;
+
+        const dateConlusion = new Date(dateInput).toISOString().split('T')[0];
 
         const newTaskData = {
             name: name,
             description: description,
-            dateConlusion: dateConlusion,
+            dateConclusion: dateConlusion,
             status: status,
             priority: priority
         };
@@ -39,59 +42,66 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-function listAll(){
-    let status;
-    let taskCompleted;
-    fetch('http://localhost:8080/task/listAll')
-        .then(response => response.json())
-        .then(data => {
-            taskDiv.innerHTML = '';
-            taskDiv.style.display = 'block';
-            data.forEach(task => {
-                if (task.status) {  
-                    status = 'Concluida';
-                    camp.checked = true;
-                } else {
-                    status = 'Pendente';
-                }
+async function listAll(){
+    try {
+        let status;
+        let taskCompleted;
+        const response = await fetch('http://localhost:8080/task/listAll');
+        const data = await response.json();
+        taskDiv.innerHTML = '';
+        taskDiv.style.display = 'block';
+                data.forEach(task => {
 
-                id = task.id;
+                    let status = task.status ? 'Concluida' : 'Pendente';
 
-                const taskElement = document.createElement('div');
-                taskElement.textContent = `${task.name} - descrição: ${task.description} - Data: ${task.dateConlusion}
-                                         - Status: ${status} - Prioridade: ${task.priority}`;
+                    const taskElement = document.createElement('div');
+                    taskElement.textContent = `${task.name} - descrição: ${task.description} - Data: ${task.dateConclusion}
+                                            - Status: ${status} - Prioridade: ${task.priority}`;
 
-                taskCompleted = document.createElement('input');
-                taskCompleted.type = 'checkbox';
-                taskCompleted.id = `taskCompleted${id}`
-                taskElement.appendChild(taskCompleted);
-                taskDiv.appendChild(taskElement);
-            });
+                    taskCompleted = document.createElement('input');
+                    taskCompleted.type = 'checkbox';
+                    taskCompleted.id = `taskCompleted-${task.id}`;
+                    taskCompleted.checked = task.status;
+                    taskElement.appendChild(taskCompleted);
+                    taskDiv.appendChild(taskElement);
+                });
+        } catch (error){
+            console.error('erro', error)
+        }
+            taskDiv.removeEventListener('change', taskCompletedUpdate)
             taskDiv.addEventListener('change', taskCompletedUpdate)
-        });
-    }
+}
 
-function taskCompletedUpdate(event){
-    const clickCheckbox = event.target;
-    const status = clickCheckbox.checked;
-    const taskId = clickCheckbox.id.split('-')[1];
 
-    const checkedStatusData = {
-        id: taskId,
-        status: status
-    };
+async function taskCompletedUpdate(event){
+        console.log('teste')
+        const clickCheckbox = event.target;
+        const status = clickCheckbox.checked;
+        const taskId = clickCheckbox.id.split('-')[1];
 
-fetch('http://localhost:8080/task//updateStatus', {
-    method: 'POST',
-    headers:{
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(checkedStatusData)
-}).then(response => {
-    if(response.status === 201) {
-        listAll();
-    } else {
-        console.error('error')
-    }
-  });
+
+        const checkedStatusData = {
+            id: taskId,
+            status: status
+        };
+//          arrumar a data que nao esta chegadno correto
+        try {
+            const response = await fetch('http://localhost:8080/task/updateStatus', {
+                method: 'POST',
+                headers:{
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(checkedStatusData)
+            }).then(response => {
+                if(response.status === 200) {
+                    listAll();
+                } else {
+                    console.error('error')
+                }
+              });
+        } catch (error) {
+            console.error('error', error)
+        }
+    
+    
 }
